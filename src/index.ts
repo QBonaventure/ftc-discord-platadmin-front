fb6028f9738275b4fde8a9d3b0f70171967ca591
Quentin Bonaventure <quentin@bonaventure.xyz>
2018-09-06 14:58:36 +0200

    Adds NoSideBar layout

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import router from './router';
 import App from './App.vue';
 
 import Default from "./layouts/Default.vue";
+import NoSideBar from "./layouts/NoSideBar.vue";
 
 Vue.component('default-layout', Default);
+Vue.component('no-side-bar-layout', NoSideBar);
 
 
 Vue.config.productionTip = false
---
+++ src/layouts/NoSideBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="wrapper">
+        <div id="header">
+            <router-link to="/" id="logo">
+                <img src="https://cdn.fearthec.io/ff491c85ad3e3aeb20723e6448ac2e2b/images/mark.png" />
+            </router-link>
+            <nav-bar />
+        </div>
+        <main>
+            <slot />
+        </main>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+    import NavBar from "./NavBar.vue";
+
+    export default Vue.extend({
+        components: {
+            NavBar
+        }
+    })
+</script>
+
+
+<style lang="scss">
+    @import 'var';
+
+    main {
+        width: 85%;
+        min-height: calc(100% - #{$header-height});
+        display: inline-block;
+        height: auto;
+        margin-top: 55px;
+    }
+</style>
+
+<style lang="scss" scoped>
+    div.wrapper {
+        height: 100%;
+    }
+</style>
+
+
