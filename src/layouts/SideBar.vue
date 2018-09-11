544e7ae463f175fbeeeb06b578a267e262b9b796
Quentin Bonaventure <quentin@bonaventure.xyz>
2018-09-11 09:31:20 +0200

    SideBar script

--- src/layouts/SideBar.vue
+++ src/layouts/SideBar.vue
@@ -5,6 +5,13 @@
     </nav>
 </template>
 
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+    })
+</script>
+
 <style lang="scss" scoped>
     @import "var";
     nav {
